36fe37eb95edad06b9b5946de7561239abc53d54
Bo <boyadeng@gmail.com>
2022-12-02 00:57:54 -0500

    grid hovering works

--- js-file.js
+++ js-file.js
@@ -1,12 +1,26 @@
-const container = document.querySelector('#container');
+const canvasWidth = 960;
+const column = 16;
+const squareCount = Math.pow(column, 2);
 
-// creates number of squares
-const squareCount = 16;
-for (let step = 0; step < squareCount; step++) {
+// sets container grid CSS
+const container = document.getElementById('container');
+const rowCount = canvasWidth / column;
+container.style.gridAutoColumns = `${rowCount}px`;
+container.style.gridAutoRows = `${rowCount}px`;
+container.style.gridTemplateColumns = `repeat(${column},${rowCount}px)`;
+
+// creates individual squares
+for (let square = 0; square < squareCount; square++) {
     const div = document.createElement('div');
-    div.id = step;
-    div.className = "step";
-    div.textContent = step;
+    div.className = "square";
+    // div.textContent = square;
     container.appendChild(div);
 }
 
+// on hover, change className from square to squareHover
+const squareArray = document.querySelectorAll(".square");
+squareArray.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.className = "squareHover";
+    });
+});
