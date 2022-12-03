481d24836b01949405f017f93deb7e4b29513660
snowbo88 <66578697+snowbo88@users.noreply.github.com>
2022-12-03 01:34:45 -0500

    rainbow drawing works
    TODO: need to work on black gradient

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function draw() {
 
 // resets canvas
 function clearCanvas() {
-    const pixels = document.querySelectorAll(".pixel, .pixelBlack, .pixelColor");
+    const pixels = document.querySelectorAll(".pixel");
     pixels.forEach((pixel) => {
         pixel.remove();
     });
@@ -50,37 +50,45 @@ function populatePixel(pixelCount) {
     }
 }
 
-// on hover, change className from pixel to pixelBlack
-// may need a switch statement here
-function pixelColorSet(pixelArray, colorClass) {
-    pixelArray.forEach((pixel) => {
-        pixel.addEventListener('mouseover', () => {
-            pixel.className = colorClass;
-        });
-    });
-}
-
 function paintPixel() {
     const pixelArray = document.querySelectorAll(".pixel");
-    var color = document.getElementById("color");
 
-    // initializes color, even when pixel density is changed
-    if (color.checked) {
-        pixelColorSet(pixelArray, "pixelColor");
-    }
-    else {
-        pixelColorSet(pixelArray, "pixelBlack");
-    }
-
-    // listen for changes
-    color.addEventListener('change', () => {
-        if (color.checked) {
-            pixelColorSet(pixelArray, "pixelColor");
-        }
-        else {
-            pixelColorSet(pixelArray, "pixelBlack");
-        }
+    pixelArray.forEach((pixel) => {
+        pixel.addEventListener('mouseover', () => {
+            if (getColorCheckbox()) {
+                pixel.style.backgroundColor = randomRGB();
+            }
+            else {
+                pixel.style.backgroundColor = "black";
+            }
+        })
     })
 }
 
 
+function randomRGB() {
+    // set style, background-color to random rgb value
+    const r = getRandomInt(0, 255);
+    const g = getRandomInt(0, 255);
+    const b = getRandomInt(0, 255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+// function from
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function getColorCheckbox() {
+    var color = document.getElementById("color");
+    if (color.checked) {
+        console.log("color");
+        return true;
+    }
+    else {
+        return false;
+    }
+}
